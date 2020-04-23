fa019071128c44c4de4c2c6a5ba9660bae19f8c2
jemrosas <jmezrosas@gmail.com>
2020-04-23 19:49:51 +0200

    Prevent submit event on searchbar when 'enter' key is pressed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,10 +7,17 @@ const SearchBar = (props) => {
       value: ev.target.value.toUpperCase(),
     });
   };
+
+  const handleSubmit = (ev) => {
+    if (ev.key === 'Enter') {
+      ev.preventDefault();
+    }
+  };
+
   return (
     <form className='form' action=''>
       <label className='form_label' htmlFor='searchField'>
-        <input className='form_input' type='text' name='searchField' id='searchField' placeholder='Insert character name' onChange={handleChange} autocomplete='off'></input>
+        <input className='form_input' type='text' name='searchField' id='searchField' placeholder='Insert character name' onChange={handleChange} onKeyPress={handleSubmit} autocomplete='off'></input>
       </label>
     </form>
   );
