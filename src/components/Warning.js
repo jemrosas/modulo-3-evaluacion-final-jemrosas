8388d3f70dd697ca76cced0a56def992b9e4fb85
jemrosas <jmezrosas@gmail.com>
2020-04-25 01:27:55 +0200

    Add styles to warning message

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../stylesheets/Warning.scss';
 import image from '../images/clipart.png';
 
 const Warning = () => {
   return (
-    <li>
-      <p>Ooopsie!!</p>
-      <img src={image} alt='Something went wrong' />
+    <li className='warning'>
+      <p className='warning__text'>Ooopsie!! Looks like something went wrong, please try another search.</p>
+      <img className='warning__image' src={image} alt='Something went wrong' />
     </li>
   );
 };
